987ca066a481c4ec919c18ba9028e48ab361088d
patrick hoehn <patrh_99@hotmail.com>
2019-11-07 23:25:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,11 +2,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name:"Lord montague",
+        name:"Lord Montague",
         age: "53"
         },
       :matriarch => {
-        name: "Lady montague",
+        name: "Lady Montague",
         age: "54"
         },
       :hero => {
